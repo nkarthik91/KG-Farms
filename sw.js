6c442bdc74c7a66d6a89f7359c510b008049247a
nkarthik91 <karthikwelcomesall@gmail.com>
2026-09-14 21:29:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kg-farms-v52';
+const CACHE='kg-farms-v55';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json','./favicon.ico','./favicon-32.png','./icons/icon-180.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-512-maskable.png','./icons/logo.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}).then(()=>self.skipWaiting()));
@@ -18,10 +18,7 @@ self.addEventListener('fetch',e=>{
     // only as an offline fallback.
     e.respondWith(
       fetch(e.request).then(res=>{
-        if(res && res.ok){
-          const copy=res.clone();
-          caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
-        }
+        if(res && res.ok) caches.open(CACHE).then(c=>c.put(e.request,res.clone()));
         return res;
       }).catch(()=>caches.match(e.request))
     );
